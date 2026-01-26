8dd3de1cb73341a2b1167e5429e61108e3153a19
Timi <timi.rautamaki@gmail.com>
2026-01-26 09:31:38 +0200

    g12a_radxa0_v1: update init commands to load dtb

--- board/amlogic/g12a_radxa0_v1/g12a_radxa0_v1.c
+++ board/amlogic/g12a_radxa0_v1/g12a_radxa0_v1.c
@@ -325,7 +325,7 @@ void aml_config_dtb(void)
 	if (MESON_CPU_MAJOR_ID_G12A != cpuid.family_id)
 		return;
 
-	run_command("fdt address $dtb_mem_addr", 0);
+	run_command("fdt addr $dtb_mem_addr", 0);
 	printf("%s %d\n", __func__, __LINE__);
 	if (cpuid.chip_rev == 0xA) {
 		printf("%s %d\n", __func__, __LINE__);
@@ -383,13 +383,13 @@ int board_late_init(void)
 		/*add board late init function here*/
 #ifndef DTB_BIND_KERNEL
 		int ret;
-		ret = run_command("store dtb read $dtb_mem_addr", 1);
+		ret = run_command("store rsv read dtb $dtb_mem_addr 0x40000", 1);
         if (ret) {
 				printf("%s(): [store dtb read $dtb_mem_addr] fail\n", __func__);
 #ifdef CONFIG_DTB_MEM_ADDR
 				char cmd[64];
 				printf("load dtb to %x\n", CONFIG_DTB_MEM_ADDR);
-				sprintf(cmd, "store dtb read %x", CONFIG_DTB_MEM_ADDR);
+				sprintf(cmd, "store rsv read dtb %x 0x40000", CONFIG_DTB_MEM_ADDR);
 				ret = run_command(cmd, 1);
                 if (ret) {
 						printf("%s(): %s fail\n", __func__, cmd);
